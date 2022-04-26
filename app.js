467033eafe97c36fa6bfba1b172318000ef5a982
KASHI TAMANG <tamang.kashi@gmail.com>
2022-04-25 18:16:14 -0700

    adds event listeners for shell 2 and 3

--- app.js
+++ app.js
@@ -27,41 +27,80 @@ let losses = 0;
 
 // set event listeners to listen for clicks on button 1
 
-button1.addEventListener('click', () =>{
+button1.addEventListener('click', () => {
+  //reset all shells on click
+    chick1.classList.add('chick');
+    chick2.classList.add('chick');
+    chick3.classList.add('chick');
+//randomizes number to choose shell 
+    const randomShell = Math.ceil(Math.random() * 3);
 //validate 
-console.log('clickkkkkkkkks!');
-  //reset shells
-const randomShell = Math.ceil(Math.random() * 3);
-//validate 
-console.log(randomShell);
+    console.log(randomShell);
 //decide which shell chick is under 
-if (randomShell === 1){
-  chick1.classList.remove('chick');
-  wins++;
-}
-//if random Shell = 1 then increment value of wins
-
-//else increment the losses
-
+    if (randomShell === 1) {
+        chick1.classList.remove('chick');
+        wins++;
+    } else if (randomShell === 2) {
+        chick2.classList.remove('chick');
+        losses++;
+    } else {
+        chick3.classList.remove('chick');
+        losses++;
+    }
 //update the wins/ loss/ totals
-winSpan.textContent = wins;
-lossesSpan.textContent = losses;
-totalSpan.textContent = wins + losses;
+    winSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
 });
 
+//set event listeners to listen for clicks on button 2
 
+button2.addEventListener('click', () => {
+    //reset all shells on click
+    chick1.classList.add('chick');
+    chick2.classList.add('chick');
+    chick3.classList.add('chick');
+  //randomizes number to choose shell 
+    const randomShell = Math.ceil(Math.random() * 3);
+    console.log(randomShell);
+  //decide which shell chick is under 
+    if (randomShell === 2) {
+        chick2.classList.remove('chick');
+        wins++;
+    } else if (randomShell === 1) {
+        chick1.classList.remove('chick');
+        losses++;
+    } else {
+        chick3.classList.remove('chick');
+        losses++;
+    }
+  //update the wins/ loss/ totals
+    winSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+});
 
-
-// set event listeners to listen for clicks on button 1
-
-
-// set event listeners to listen for clicks on button 1
-
-
-
-
-
-
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+//set event listeners to listed for clicks on button 3
+button3.addEventListener('click', () => {
+    //reset all shells on click
+    chick1.classList.add('chick');
+    chick2.classList.add('chick');
+    chick3.classList.add('chick');
+  //randomizes number to choose shell 
+    const randomShell = Math.ceil(Math.random() * 3);
+  //decide which shell chick is under 
+    if (randomShell === 3) {
+        chick3.classList.remove('chick');
+        wins++;
+    } else if (randomShell === 2) {
+        chick2.classList.remove('chick');
+        losses++;
+    } else {
+        chick1.classList.remove('chick');
+        losses++;
+    }
+  //update the wins/ loss/ totals
+    winSpan.textContent = wins;
+    lossesSpan.textContent = losses;
+    totalSpan.textContent = wins + losses;
+});
